a0d3d44d6858fb9f55125832cf2b52cc1dc98106
yiqwan <tyq2708@hotmail.com>
2023-11-07 00:01:53 +0800

    Created class PricingModel
    Did some refactor also like slicing the dates

--- code/yq/model.py
+++ code/yq/model.py
@@ -1,5 +1,47 @@
 import pandas as pd
 
-class MultidimensionalGBM:
-    def __init__(self, df: pd.DataFrame):
-        self.df = df
+# In the main codes, we can create different models, and at the end access the sim_data to plot all the payoff_df aaginst the product prices
+class PricingModel:
+    def __init__(*, self, hist_data):
+        self.hist_data = hist_data # Historical data for the underlying assets
+        self.sim_data = None # To store simulated paths of the underlying assets
+        self.payoff_df = None # To store payoff calculations based on simulated data
+
+    def multidimensional_gbm(self, parameters):
+        # Implementation of the multidimensional Geometric Brownian Motion model
+        pass
+
+    def interest_rate_model(self, parameters):
+        # Implementation of the interest rate model (e.g., Vasicek, CIR)
+        pass
+    
+    def heston_model(self, parameters):
+        # Implementation of the Heston model for option pricing
+        pass
+
+    def monte_carlo_simulation(self, parameters):
+        # General Monte Carlo simulation method
+        pass
+    
+    def apply_control_variate(self, control_parameters):
+        # Method to apply control variate techniques to reduce simulation variance
+        pass
+
+    def calculate_payoff_all_features(self, parameters):
+        # Method to calculate payoff with all features like barrier, autocallable, stock conversion
+        pass
+
+    def calculate_payoff_barrier(self, parameters):
+        # Method to calculate payoff with barrier feature only (for eg.)
+        pass
+
+    def plotSimulatedData(some_paramenters):
+        # Not sure whether this should be subplot to compare with other pricing models or a plot for this current model only
+        pass
+
+# Example usage:
+hist_stock_data = pd.DataFrame(...)  # This would be your historical stock data
+pricing_model = PricingModel(hist_stock_data)
+# Call methods as needed:
+# pricing_model.multidimensional_gbm(some_gbm_parameters)
+# pricing_model.apply_control_variate(control_variate_parameters)
