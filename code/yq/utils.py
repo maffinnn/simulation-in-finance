c1f83f34a6f6c66b140d0959178d2c0b8965fd3b
yiqwan <tyq2708@hotmail.com>
2023-11-07 15:52:47 +0800

    Refactored generate trading day function

--- code/yq/utils.py
+++ code/yq/utils.py
@@ -1,6 +1,20 @@
 import typing
 import pandas as pd
 import constants as cs
+import pandas_market_calendars as mcal
+
+# Takes the start and end dates to create a trading calendar for SIX
+# Returns a df with all the dates of trading days as index
+def create_six_trading_dates(start_date, end_date):
+    six_calendar = mcal.get_calendar('SIX')
+    print(f"Holidays in the calendar up to 2200: {six_calendar.holidays().holidays[-10:]}")
+    six_trading_days = six_calendar.valid_days(start_date, end_date)
+
+    six_trading_days_df = pd.DataFrame(index = six_trading_days)
+    
+    # Name the index "Dates"
+    six_trading_days_df.index.name = 'Dates'
+    return six_trading_days_df
 
 # Input: a dataframe ith dates as index
 def remove_SIX_holidays(data: pd.DataFrame) -> pd.DataFrame:
