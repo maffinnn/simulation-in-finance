bd18f050742ecded95f738f092696387759e9d99
maffinnn <biansiyuanlynn@gmail.com>
2023-11-07 19:18:07 +0800

    add interest rate

--- code/sy/interest_rate.py
+++ code/sy/interest_rate.py
@@ -5,24 +5,38 @@ import numpy as np
 import pandas as pd
 
 # interest rate model
-class CIR(object):
+class VasicekModel(object):
     def __init__(self, data: pd.DataFrame, params: typing.Dict):
-        # TODO
-        self.data = data
         """
         b: long term mean level: All future trajectories of r will evolve around a mean level b in the long run.
         a: speed of reversion: A characterizes the velocity at which such trajectories will regroup around b.
         sigma: instantaneous volatility: measures instant by instant the amplitude of randomness
+        Vasicek closed form: r(t) = r(t-1)exp(-a*dt) + b*(1-exp(-a*dt)) + sigma * exp(-a*dt) 
         """
-        self.a = None
-        self.b = None
-        self.sigma = None
-        return None
-
-    def fit(self, current_date: str):
-        # TODO
-        return self
+        self.data = data
+        self.a = params.get('speed of reversion') # 0
+        self.b = params.get('long term mean level') # 0.107659718380514
+        self.sigma = params.get('sigma') # 0.106212663278328
+        self.maturity_date = params.get('maturity_date')
+        self.dt = 1/252
     
-    def generate_path(self):
-        # TODO
-        return None
+    def generate_path(self, current_date: str)->pd.DataFrame:
+        """
+            N: the number steps in the path
+        """
+        N = (pd.to_datetime(self.maturity_date) - pd.to_datetime(current_date)).days + 1
+        Rt = [0]*(N+1)
+        prev_date = pd.to_datetime(current_date) + pd.DateOffset(days=1)
+        Rt[0] = self.data.loc[prev_date]['Price']
+        for i in range(1, N+1):
+            Rt[i] = self.a*(self.b-Rt[i-1]) * self.dt + self.sigma * np.random.normal(0, np.sqrt(self.dt)) + Rt[i-1]
+        Rt = Rt[1:]
+        return pd.DataFrame(data=Rt, index=pd.date_range(current_date, self.maturity_date), columns=['Rate'])
+
+
+class ConstantRateModel(object):
+    def __init__(self, data: pd.DataFrame, params: typing.Dict):
+        self.data = data
+    
+    def get_rate(self):
+        return self.data['Price'].mean()
