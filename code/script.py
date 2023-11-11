fd57763fdac558118a6fe5161c198d34e7260948
yiqwan <tyq2708@hotmail.com>
2023-11-11 18:08:39 +0800

    Refactored heston model

--- code/script.py
+++ code/script.py
@@ -1,11 +1,27 @@
-import os
-print(os.getcwd())
-from yq.utils import option
-from yq.utils import calendar
+import typing
+import pickle
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import QuantLib as ql
+import yfinance as yf
+import pandas_market_calendars as mcal
+import datetime
+import time
+from pathlib import Path
+import os
+
+# Self-written modules
 from yq.scripts import models
 from yq.scripts import heston_func
+from yq.utils import option
+from yq.utils import calendar
+from yq.scripts import simulation as sm
 from sc import constants as cs
+from sy.variance_reduction import apply_control_variates
+from sy.interest_rate import populate_bond_table, get_period
+from sy.calibration import apply_empirical_martingale_correction
 
 # data = option.read_options_data("lonn_call.csv")
 # print(data)
@@ -20,8 +36,82 @@ from sc import constants as cs
 # business_days = trading_calendar.calculate_business_days(start_date, end_date)
 # print(f"Number of business days: {business_days}")
 
+# Serialize and save an object
+def save_object(obj, filename):
+    with open(filename, 'wb') as outp:  # Write in binary mode
+        pickle.dump(obj, outp, pickle.HIGHEST_PROTOCOL)
+
+# Load a previously saved object
+def load_object(filename):
+    with open(filename, 'rb') as inp:  # Read in binary mode
+        return pickle.load(inp)
+
 
 if __name__ == "__main__":
+    historical_start_date = '2022-08-09'
+    # Define the ticker list
+    ticker_list = ['LONN.SW', 'SIKA.SW']
+
+    # Fetch the data
+    data = yf.download(ticker_list, historical_start_date)['Adj Close'] # Auto adjust is false
+    data.plot()
+    
+    # model_instance = models.PricingModel(params=params)
+    # save_object(model_instance, 'model_instance.pkl')
+    # loaded_model_instance = load_object('model_instance.pkl')
+    # print(loaded_model_instance.data)
+
+    start_time_acc = datetime.datetime.now() # Track the nth attempt
+    print(start_time_acc)
+    n_sim = 1
+    trading_calendar = calendar.SIXTradingCalendar()
+    bus_date_range = trading_calendar.create_six_trading_dates('2023-08-09', '2023-08-09')
+    for prod_date in bus_date_range.index:
+        params = {
+            'data': data,
+            'ticker_list': ['LONN.SW', 'SIKA.SW'],
+            'prod_date': prod_date
+        }
+        try:
+            start_time = time.time()
+            heston = models.PricingModel(params=params)
+
+            sim_data_df = []
+            for sim in range(n_sim):
+                sim_start_date = trading_calendar.add_trading_day(prod_date, 1)
+                
+                
+                sim_data = heston.multi_asset_heston_model(
+                    sim_start_date=sim_start_date, 
+                    hist_window=252, 
+                    sim_window=trading_calendar.calculate_business_days(sim_start_date, cs.FINAL_FIXING_DATE), 
+                    h_adjustment=[0, 0])
+                sim_data_df.append(sim_data)
+                
+                sim_data_h = heston.multi_asset_heston_model(
+                    sim_start_date=sim_start_date, 
+                    hist_window=252, 
+                    sim_window=trading_calendar.calculate_business_days(sim_start_date, cs.FINAL_FIXING_DATE), 
+                    h_adjustment=[1, 0])
+
+            end_time = time.time()
+            elapsed_time = end_time - start_time
+            min, sec = divmod(elapsed_time, 60)
+            print(f"The elapsed time is for {n_sim} is {int(min)} minutes, {int(sec)} seconds")
+            
+            S_T_1 = sim_data.loc[cs.FINAL_FIXING_DATE, 'LONN.SW']
+            S_T_2 = sim_data_h.loc[cs.FINAL_FIXING_DATE,'LONN.SW']
+            print(S_T_1 / S_T_2)
+            
+            S_0_1 = data.loc[prod_date, 'LONN.SW']
+            S_0_2 = data.loc[prod_date, 'LONN.SW'] + 1
+            print(S_0_1 / S_0_2)
+            print(S_T_1, S_T_2, S_0_1, S_0_2)
+        except Exception as e:
+            # Log the error with the date that caused it
+            raise Exception("MultiHeston has error.")
+        
+    pass
     # calendar = calendar.SIXTradingCalendar()
     # dates = calendar.create_six_trading_dates('2023-08-09', '2023-11-09')
     # for date in dates.index:
--- code/yq/scripts/models.py
+++ code/yq/scripts/models.py
@@ -14,11 +14,15 @@ class PricingModel:
         self.time_steps_per_year = 252
         self.dt = 1/self.time_steps_per_year
         self.num_ticker = len(self.ticker_list) # Number of stocks
+        self.prod_date = params.get('prod_date')
+        self.S_0_vector = None
         self.sim_data = None # To store simulated paths of the underlying assets
         self.payoff_df = None # To store payoff calculations based on simulated data
         self.Z_list = None
         self.Z_list_heston = None
         self.params_list_heston = None
+        self.L_lower = None
+        
     
     # Implementation of the multi-asset Geometric Brownian Motion model
     def multi_asset_gbm(self, sim_start_date: pd.Timestamp, hist_window: int, 
@@ -136,91 +140,94 @@ class PricingModel:
         # Do calculations on r, volatility, rho etc.?
     
         # Get the correlation between the log returns of the Si
-        try:
-            last_avai_price_date = self.calendar.add_trading_day(sim_start_date, -1)
-            S_0_vector = [self.data.loc[last_avai_price_date, self.ticker_list[i]] + h_adjustment[i]
-                    for i in range(self.num_ticker)] # Stock price of the 0th day of simulation            
-            hist_data = self.data[self.data.index < sim_start_date].tail(hist_window)
-            print(f"S_0_vector is {S_0_vector}\n")
-        except Exception as e:
-            raise Exception("Error at wrangling historical data.")
+        if self.S_0_vector is None:
+            try:
+                last_avai_price_date = self.calendar.add_trading_day(sim_start_date, -1)
+                self.S_0_vector = [self.data.loc[last_avai_price_date, self.ticker_list[i]] + h_adjustment[i]
+                        for i in range(self.num_ticker)] # Stock price of the 0th day of simulation            
+                hist_data = self.data[self.data.index < sim_start_date].tail(hist_window)
+                print(f"S_0_vector is {self.S_0_vector}\n")
+            except Exception as e:
+                raise Exception("Error at wrangling historical data.")
 
-        try:
-            # TODO: Change to calibrate every day if have time, hardcoded the calibration S_t
-            if (self.params_list_heston is None):
-                # Read options data 
-                lonn_call = option.read_options_data("lonn_call.csv")
-                print(f"Lonza options:\n{lonn_call}")
+        if (self.params_list_heston is None):
+            try:
+                # TODO: Change to calibrate every day if have time, hardcoded the calibration S_t
+                    # Read options data 
+                    lonn_call = option.read_options_data("lonn_call.csv")
+                    print(f"Lonza options:\n{lonn_call}")
 
-                sika_call = option.read_options_data("sika_call.csv")
-                print(f"Sika options:\n{sika_call}")
+                    sika_call = option.read_options_data("sika_call.csv")
+                    print(f"Sika options:\n{sika_call}")
 
-                # Calibrate 2 sets of parameters for 2 individual assets
-                start_time = time.time()
+                    # Calibrate 2 sets of parameters for 2 individual assets
+                    start_time = time.time()
 
-                lonn_result = hf.calibrate_heston(self.data.loc['2023-11-07']['LONN.SW'], lonn_call)
-                sika_result = hf.calibrate_heston(self.data.loc['2023-11-07']['SIKA.SW'], sika_call)
-                end_time = time.time()
-                elapsed_time = end_time - start_time
-                min, sec = divmod(elapsed_time, 60)
-                print(f"The elapsed time for 2 calibration is {int(min)} minutes, {int(sec)} seconds")
+                    lonn_result = hf.calibrate_heston(self.data.loc['2023-11-07']['LONN.SW'], lonn_call)
+                    sika_result = hf.calibrate_heston(self.data.loc['2023-11-07']['SIKA.SW'], sika_call)
+                    end_time = time.time()
+                    elapsed_time = end_time - start_time
+                    min, sec = divmod(elapsed_time, 60)
+                    print(f"The elapsed time for 2 calibration is {int(min)} minutes, {int(sec)} seconds")
+            
+                    self.params_list_heston = np.zeros((self.num_ticker, 5))
+                    params_order = ['kappa', 'theta', 'volvol', 'rho', 'sigma']
+                    # print(f"Enumerate order is: {enumerate(params_order)}\n")
+                    for i, param in enumerate(params_order):
+                        self.params_list_heston[0, i] = lonn_result.params[param].value  # For lonn_result
+                        self.params_list_heston[1, i] = sika_result.params[param].value  # For sika_result
+
+                    np.set_printoptions(suppress=True, precision=4)  # 'precision' controls the number of decimal places
+                    print(f"The parameters list for Heston is:\n{self.params_list_heston}")
+            except Exception as e:
+                raise Exception("Error at calibrating Hestonmodel parameters.")
+
+            # Calculate the rho_S1S2
+            try:
+                log_returns_list = []
+                for ticker in self.ticker_list:
+                    # print(data[ticker], data[ticker].shift(1))
+                    log_returns = np.log(hist_data[ticker] / hist_data[ticker].shift(1)) # np.log is natural log, (P_i/P_i-1)
+                    log_returns.dropna(inplace = True) # A series
+                    log_returns_list.append(log_returns)
+                    # print(type(log_returns))
+                corr_matrix = np.corrcoef(log_returns_list)
+                rho_S1S2 = corr_matrix[0][1]
+                # print(f"rho_S1S2 is {rho_S1S2}\n")
+                # print(f"Correlation between log returns of each asset:\n{corr_matrix}\n")
+
+            except Exception as e:
+                raise Exception("Error at calculating the correlation.")
         
-                self.params_list_heston = np.zeros((self.num_ticker, 5))
-                params_order = ['kappa', 'theta', 'volvol', 'rho', 'sigma']
-                # print(f"Enumerate order is: {enumerate(params_order)}\n")
-                for i, param in enumerate(params_order):
-                    self.params_list_heston[0, i] = lonn_result.params[param].value  # For lonn_result
-                    self.params_list_heston[1, i] = sika_result.params[param].value  # For sika_result
+            # Calculate the Cholesky matrix
+            try:
+                rho_S1V1, rho_S2V2 = self.params_list_heston[0][3], self.params_list_heston[1][3]
+                simplified_corr = np.array([
+                    [1., rho_S1V1, rho_S1S2, 0.],
+                    [rho_S1V1, 1., 0., 0.],
+                    [rho_S1S2, 0., 1., rho_S2V2],
+                    [0., 0., rho_S2V2, 1.]
+                ])
 
-                np.set_printoptions(suppress=True, precision=4)  # 'precision' controls the number of decimal places
-                # print(f"The parameters list for Heston is:\n{self.params_list_heston}")
-        except Exception as e:
-            raise Exception("Error at calibrating Hestonmodel parameters.")
+                # print(f"Simplified correlation matrix is:\n{simplified_corr}\n")
 
-        # Calculate the rho_S1S2
-        try:
-            log_returns_list = []
-            for ticker in self.ticker_list:
-                # print(data[ticker], data[ticker].shift(1))
-                log_returns = np.log(hist_data[ticker] / hist_data[ticker].shift(1)) # np.log is natural log, (P_i/P_i-1)
-                log_returns.dropna(inplace = True) # A series
-                log_returns_list.append(log_returns)
-                # print(type(log_returns))
-            corr_matrix = np.corrcoef(log_returns_list)
-            rho_S1S2 = corr_matrix[0][1]
-            # print(f"rho_S1S2 is {rho_S1S2}\n")
-            # print(f"Correlation between log returns of each asset:\n{corr_matrix}\n")
+                self.L_lower = np.linalg.cholesky(simplified_corr)
 
-        except Exception as e:
-            raise Exception("Error at calculating the correlation.")
+                # print(f"The lower triangular matrix by Cholesky decomposition is:\n {L_lower}\n")
+            
+            except Exception as e:
+                raise Exception("Error at calculating Cholesky.")
         
-        # Calculate the Cholesky matrix
-        try:
-            rho_S1V1, rho_S2V2 = self.params_list_heston[0][3], self.params_list_heston[1][3]
-            simplified_corr = np.array([
-                [1., rho_S1V1, rho_S1S2, 0.],
-                [rho_S1V1, 1., 0., 0.],
-                [rho_S1S2, 0., 1., rho_S2V2],
-                [0., 0., rho_S2V2, 1.]
-            ])
-
-            # print(f"Simplified correlation matrix is:\n{simplified_corr}\n")
-
-            L_lower = np.linalg.cholesky(simplified_corr)
-
-            # print(f"The lower triangular matrix by Cholesky decomposition is:\n {L_lower}\n")
-        
-        except Exception as e:
-            raise Exception("Error at calculating Cholesky.")
-        
-        # Generate random variable
+        # Generate random variable, assume the original path must be simulated before
+        # simulating other +h, -h paths for each asset
         if h_adjustment == [0, 0]:
             self.Z_list_heston = np.random.normal(0, 1, (self.num_ticker * 2, sim_window))
         print(f"Z_list_heston is:\n {self.Z_list_heston}\n")
         # Perform heston for each time step, each asset (diff set of params)
+        
         try:
             sim_data = pd.DataFrame(np.zeros((sim_window, self.num_ticker)), columns = [self.ticker_list])
-            S_t_vector = S_0_vector # Price vector t (to be updated after every step)
+            S_t_vector = self.S_0_vector # Price vector t (to be updated after every step)
             V_t_vector = self.params_list_heston[:, 4] # Initial V_0 (to be updated after every step also)
             for t in range(sim_window):
                 Z = self.Z_list_heston[:, t]
@@ -231,7 +238,7 @@ class PricingModel:
                     xi = self.params_list_heston[i][2]
                     V_t = V_t_vector[i]
                     
-                    LZ = np.dot(L_lower, Z.T) # For 1D vector the transpose doesn't matter, but for higher dimension yes
+                    LZ = np.dot(self.L_lower, Z.T) # For 1D vector the transpose doesn't matter, but for higher dimension yes
                     # print(f"The 3 matrices are: \n", L_lower, Z.T, LZ)
 
                     S_t_vector[i] = S_t * np.exp((interest_rate - 0.5 * V_t) * self.dt + np.sqrt(V_t) * np.sqrt(self.dt) * LZ[2 * i])
