ee8dc8270a32c88daa82c5fee1b8b3f7da9ce3d4
maffinnn <biansiyuanlynn@gmail.com>
2023-11-18 10:38:26 +0800

    minor change

--- code/sy/variance_reduction.py
+++ code/sy/variance_reduction.py
@@ -17,10 +17,9 @@ def apply_control_variates(S1T_n, S2T_n, mu1, mu2, payouts_n):
     ST_n_est = np.array([S1T_n_est, S2T_n_est])
     payouts_n_reg = payouts_n[:reg_len]
     payouts_n_est = payouts_n[reg_len:]
-
-    reg = LinearRegression().fit(np.transpose(ST_n_reg), payouts_n_reg)
-    coef = reg.coef_
     mu = np.array([[mu1, mu2] for _ in range(N-reg_len)])
+    reg = LinearRegression().fit(np.transpose(mu), payouts_n_reg)
+    coef = reg.coef_
     payouts_cv = payouts_n_est + np.dot((mu - np.transpose(ST_n_est)), coef)
     return payouts_cv
 
